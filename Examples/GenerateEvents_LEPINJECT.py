255bf06d1af000c3dd27d07018e748be86187f2a
tmcelroy <tmcelroy@ualberta.ca>
2021-12-13 15:15:51 -0700

    moved the neutrino generator to lepton injector and fixed Proposal_config.json to remove hardcoding to my personal folder.

--- Examples/GenerateEvents_LEPINJECT.py
+++ Examples/GenerateEvents_LEPINJECT.py
@@ -1,9 +1,8 @@
 from I3Tray import *
-from icecube import icetray, dataclasses, phys_services, sim_services, dataio,  earthmodel_service, neutrino_generator, tableio, hdfwriter
-from icecube.simprod import segments
-from icecube.icetray import I3Units, I3Frame
-from icecube.dataclasses import I3Particle
-from icecube.simclasses import I3MMCTrack
+from icecube import icetray, dataio, dataclasses
+from icecube import phys_services
+from icecube import LeptonInjector 
+from icecube.icetray import I3Units
 from icecube import PROPOSAL
 import os
 from os.path import expandvars
@@ -20,8 +19,7 @@ parser.add_argument('-r',    '--runNum',    default = 0,                        
 parser.add_argument("-a",    "--ratios",    default="1.0:1.0:1.0:1.0:1.0:1.0",                                help="ratio of input neutrino")
 parser.add_argument("-t",    "--types",     default="NuE:NuEBar:NuTau:NuTauBar:NuMu:NuMuBar",                      help="type of input neutrino")
 parser.add_argument("-g",    "--gcd",       default=os.getenv('PONESRCDIR')+"/GCD/PONE_Phase1.i3.gz", help="gdc file")
-parser.add_argument("-c",    "--crossdir",  default=os.getenv('PONESRCDIR')+"/CrossSectionModels",    help='path to cross section models')
-parser.add_argument("-m",    "--crossmodel",default='csms_differential_v1.0',                         help='cross section model')
+parser.add_argument("-c",    "--crossdir",  default=os.getenv('PONESRCDIR')+"/CrossSectionModels/csms_differential_v1.0",    help='path to cross section models')
 
 args = parser.parse_args()
 
@@ -51,16 +49,10 @@ gcd = args.gcd
 tray = I3Tray()
 
 #Random
-randomService = phys_services.I3SPRNGRandomService(
-        seed = 1234657,
-		nstreams = 10000,
-        streamnum = runNum)
-tray.context['I3RandomService'] = randomService
-tray.Add("I3InfiniteSource", prefix = gcd)
-
-tray.Add("I3MCEventHeaderGenerator",
-	       EventID=1,
-	       IncrementEventID=True)
+randomService = phys_services.I3GSLRandomService(seed=args.runNum)
+tray.context["I3RandomService"] = randomService
+tray.AddService("I3EarthModelServiceFactory", "Earth")
+tray.AddModule("I3InfiniteSource", "TheSource", Stream=icetray.I3Frame.DAQ)
 
 tray.Add("I3EarthModelServiceFactory", "EarthModelService",
                 EarthModels = ["PREM_mmc"],
@@ -69,58 +61,62 @@ tray.Add("I3EarthModelServiceFactory", "EarthModelService",
                 DetectorDepth = 2600*I3Units.m,
                 PathToDataFileDir = "")
 
-tray.Add("I3NuGSteeringFactory", "steering",
-                EarthModelName = "EarthModelService",
-                NEvents = numEvents,
-                SimMode = "Detector",
-                VTXGenMode = "NuGen",
-                InjectionMode = "surface",
-                CylinderParams = cylinder,
-                DoMuonRangeExtension = False,
-                UseSimpleScatterForm = True,
-                MCTreeName = "I3MCTree_NuGen"
-                )
+# we create a list of injector objects
+#   each of these injectors can have a unique cross sections used 
+injector_list = []
+injector_list.append(
+    LeptonInjector.injector(
+        NEvents         = 100,
+        FinalType1      = dataclasses.I3Particle.ParticleType.MuMinus,
+        FinalType2      = dataclasses.I3Particle.ParticleType.Hadrons,
+        DoublyDifferentialCrossSectionFile  = xs_folder + "/dsdxdy_nu_CC_iso.fits",
+        TotalCrossSectionFile               = xs_folder + "/sigma_nu_CC_iso.fits",
+        Ranged = True)
+    )
+injector_list.append(
+    LeptonInjector.injector(
+        NEvents         = 100,
+        FinalType1      = dataclasses.I3Particle.ParticleType.EMinus,
+        FinalType2      = dataclasses.I3Particle.ParticleType.Hadrons,
+        DoublyDifferentialCrossSectionFile  = xs_folder + "/dsdxdy_nu_CC_iso.fits",
+        TotalCrossSectionFile               = xs_folder + "/sigma_nu_CC_iso.fits",
+        Ranged = False)
+    )
 
-tray.Add("I3NuGDiffuseSource","diffusesource",
-               RandomService = randomService,
-               SteeringName = "steering",
-		#NuFlavor = 'NuTau',
-               NuTypes = typevec,#['NuTau','NuTauBar'],
-               PrimaryTypeRatio = ratiovec,
-               GammaIndex = 2.19,
-               EnergyMinLog = emin,
-               EnergyMaxLog = emax,
-               ZenithMin = zenithMin,
-               ZenithMax = zenithMax,
-               AzimuthMin = azimuthMin,
-               AzimuthMax = azimuthMax,
-               ZenithWeightParam = 1.0,
-               AngleSamplingMode = "COS"
-              )
-
-tray.Add("I3NuGInteractionInfoDifferentialFactory", "interaction",
-                RandomService = randomService,
-                SteeringName = "steering",
-                TablesDir = args.crossdir,
-                CrossSectionModel = args.crossmodel
-              )
-
-tray.Add("I3NeutrinoGenerator","generator",
-                RandomService = randomService,
-                SteeringName = "steering",
-                InjectorName = "diffusesource",
-                InteractionInfoName = "interaction",
-                #PropagationWeightMode = "NCGRWEIGHTED",
-                InteractionCCFactor = 1.0,
-                InteractionNCFactor = 1.0,
-                #InteractionGRFactor = 1.0
-              )
+# Create the multileptoninjector object with your list of injectors 
+tray.AddModule("MultiLeptonInjector",
+    EarthModel      = "Earth",
+    Generators      = injector_list,
+    MinimumEnergy   = 100. * I3Units.GeV,
+    MaximumEnergy   = (1e6) * I3Units.GeV,
+    MinimumZenith   = 90 * I3Units.deg,
+    MaximumZenith   = 180 * I3Units.deg, 
+    PowerLawIndex   = 2.,
+    InjectionRadius = 1200 * I3Units.meter,
+    EndcapLength    = 1200 * I3Units.meter,
+    CylinderRadius  = 800 * I3Units.meter,
+    CylinderHeight  = 800 * I3Units.meter,
+    MinimumAzimuth  = 0. * I3Units.deg,
+    MaximumAzimuth  = 360. * I3Units.deg,
+    RandomService   = "I3RandomService")
 
 tray.Add(segments.PropagateMuons, 'ParticlePropagators',
          RandomService=randomService,
          SaveState=True,
          InputMCTreeName="I3MCTree_NuGen",
          OutputMCTreeName="I3MCTree")
+
+event_id = 1
+def get_header(frame):
+    global event_id 
+    header          = dataclasses.I3EventHeader()
+    header.event_id = event_id
+    header.run_id   = seed
+    frame["I3EventHeader"] = header
+
+    event_id += 1
+
+tray.AddModule(get_header, streams = [icetray.I3Frame.DAQ])
 
 #converts q frames?
 tray.Add("I3NullSplitter",
