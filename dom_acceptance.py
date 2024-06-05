3f29c47d47aea9d3d55269ddfb5f54c2d0c643c0
Christian Haack <christian.haack@fau.de>
2024-06-05 13:03:58 +0200

    move dom acceptance test script

--- dom_acceptance.py
+++ dom_acceptance.py
@@ -3,7 +3,10 @@ import numpy as np
 import numpy as np
 from scipy.stats import rayleigh
 import matplotlib.pyplot as plt
+import os
+
 dom_properties = DOMUtility.DOMProperties()
+
 
 def GetPMT(photonDir, wl, weight):
         random = np.random.uniform()
@@ -92,22 +95,54 @@ for i, (cos_zen, azi) in enumerate(zip(cos_zens, azis)):
     pmt_ixs[i] = GetPMT(photon_dir, wl, 1)
 
 # Calculate acceptance
-accepted = np.sum(pmt_ixs > 0)
+accepted = np.sum(pmt_ixs >= 0)
 print("Old code: ", accepted / n)
 
-pmt_acc = DOMUtility.Geant4PMTAcceptance("/home/chrhck/repos/pone_offline/data/pmt_acc.npz")
+pmt_acc = DOMUtility.Geant4PMTAcceptance(os.getenv("PONESRCDIR") +"/data/pmt_acc.npz")
 rel_positions = np.array([[np.sin(np.arccos(cz)) * np.cos(a), np.sin(np.arccos(cz)) * np.sin(a), cz] for cz, a in zip(cos_zens, azis)])
 wavelengths = np.full(n, wl)
 
 pmt_ixs_new = pmt_acc.check_pmt_hit(rel_positions, wavelengths, np.ones_like(wavelengths))
 
-plt.hist(pmt_ixs[pmt_ixs > 0], bins=np.arange(1, 18))
-plt.savefig("pmt_ixs.png")
-
-
-
-
-
-
 accepted = np.sum(pmt_ixs_new > 0)
 print("New code: ", accepted / n)
+
+#dom_properties.MakePMTAcceptancePlots(".")
+pmt_acc = np.load(os.getenv("PONESRCDIR") + "/data/ang_acceptance_16_all.npy")
+
+plt.figure()
+plt.imshow(np.sum(pmt_acc, axis=0).T / dom_properties.maxAngularAcceptance, extent=[0, 2*np.pi, 0, np.pi], cmap=plt.cm.viridis, origin="lower", aspect="auto")
+plt.xlabel("Phi (rad)")
+plt.ylabel("Theta (rad)")
+plt.colorbar(label="Bin Entry")
+plt.savefig("PMTAcceptance_sum.png")
+
+"""
+from icecube import dataio, dataclasses, simclasses, icetray
+
+hdl = dataio.I3File("GenerateSingleMuons_100_photonprop.i3.gz")
+ix_photons_map = {}
+while hdl.more():
+
+    fr = hdl.pop_frame()
+    if fr.Stop == icetray.I3Frame.DAQ:
+        photons = fr["I3Photons"]
+        ix_photons_map[fr["I3EventHeader"].event_id] = sum([len(ps) for key, ps in photons])
+hdl.close()
+
+ix_photons_split_map = {}
+hdl = dataio.I3File("GenerateSingleMuons_100_photonprop_daqSim_noise_ON.i3.gz")
+while hdl.more():
+    
+    fr = hdl.pop_frame()
+    if fr.Stop == icetray.I3Frame.DAQ:
+        pmt_split_map = fr["I3Photons_pmtsplit"]
+        print(fr["I3Photons_pmtsplit"])
+        ix_photons_split_map[fr["I3EventHeader"].event_id] = sum([len(ps) for key, ps in pmt_split_map])
+
+hdl.close()
+
+for key in ix_photons_map.keys():
+    if key in ix_photons_split_map:
+        print(key, ix_photons_map[key], ix_photons_split_map[key])
+"""
