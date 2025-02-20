7a7be7d90bfafae9afdfe0e5952a7aa1f5cf6ec4
Caleb Miller <calebmiller@uvic.ca>
2025-02-20 14:17:16 -0800

    added docstrings

--- GCD/Generate_from_csv.py
+++ GCD/Generate_from_csv.py
@@ -6,16 +6,10 @@ import gcdHelpers
 import csv
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--input", type=str, default=None, help="csv file to read")
+parser.add_argument("-i","--input",  type=str, default=None, help="csv file to read")
 parser.add_argument("-o","--output", type=str, default="out", help="i3 file name to write")
-parser.add_argument("-d", "--ndoms", type=int, default=20, help="Doms per string.")
-parser.add_argument(
-    "-r",
-    "--domradius",
-    type=int,
-    default=(17.0 * 2.54 * 0.01 * 0.5),
-    help='Radius of dom. Defaults to 17"',
-)
+parser.add_argument("-d","--ndoms",  type=int, default=20, help="Doms per string.")
+parser.add_argument("-r","--domradius", type=int, default=(17.0 * 2.54 * 0.01 * 0.5), help='Radius of dom. Defaults to 17"')
 parser.add_argument("-p", "--npmts", type=int, default=16, help="PMTs per DOM.")
 args = parser.parse_args()
 
@@ -28,24 +22,33 @@ outfile = dataio.I3File(outfileName, "w")
 domsPerString = args.ndoms
 
 def generateGeometry():
+    '''
+    Creates an I3OMGeoMap object to return, created from x,y coordinates in a csv file
+
+            Parameters:
+                    None
+            Returns:
+                    I3OMGeoMap: map of Optical Modules (OMs)
+    '''
     global domsPerString
     global spacing
 
-    orientation = dataclasses.I3Orientation(0, 0, -1, 1, 0, 0)
-    area = 4.0 * ((args.domradius) ** 2.0) * np.pi * I3Units.meter2
+    orientation = dataclasses.I3Orientation(0, 0, -1, 1, 0, 0) #not sure what this does, but it was in other files
+    area = 4.0 * ((args.domradius) ** 2.0) * np.pi * I3Units.meter2 
     geomap = dataclasses.I3OMGeoMap()
 
 
+    #create list of depths for modules, currently hardcoded. Should add to params
     sp = 950.0 / 19.0
     depthlist = [(-450.0 + sp * i) * I3Units.meter for i in range(20)]
-    depth = np.array(depthlist)
+    depth = np.array(depthlist) 
 
 
-
+    #read x,y from csv file
     xpositions = []
     ypositions = []
 
-    if(args.input==None): raise Exception("invalid filename")
+    if(args.input==None): raise Exception("no csv input file specified") 
 
 
     with open(args.input, newline='') as csvfile:
@@ -55,6 +58,7 @@ def generateGeometry():
             xpositions.append(float(row[0]))
             ypositions.append(float(row[1]))
 
+    #Add OMs at x,y and depth
     for i in range(len(xpositions)):
         for m in range(domsPerString):
             omGeometry = dataclasses.I3OMGeo()
