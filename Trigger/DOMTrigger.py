cf9eb0286ee80cce583fa5765349ab7b15c51e22
JakubStacho <jakubs@sfu.ca>
2025-05-21 15:05:29 -0700

    Added ability to choose trigger map in the DOMTrigger and renamed default output

--- Trigger/DOMTrigger.py
+++ Trigger/DOMTrigger.py
@@ -16,9 +16,8 @@ class DOMTrigger(icetray.I3ConditionalModule):
     def __init__(self, context):
         icetray.I3ConditionalModule.__init__(self, context)
         self.AddParameter("output", "Append the outputs", "")
-        self.AddParameter(
-            "inputmap", "Name of the Physics I3MCTree name", "I3RecoPulseSeriesMap"
-        )
+        self.AddParameter("inputmap", "Name of the Physics I3MCTree name", "I3RecoPulseSeriesMap")
+        self.AddParameter("trigger_map", "Name of om wide pulse series map to trigger off of", "triggerpulsemap")
         self.AddParameter("PEthreshold", " Pulse charge threshold", 0.25)
         self.AddParameter("CutNotTriggered", "Cut events that do not trigger.", False)
         self.AddParameter("SingleDOMCoincidenceN", "", 3)
@@ -28,14 +27,13 @@ class DOMTrigger(icetray.I3ConditionalModule):
         self.AddOutBox("OutBox")
 
     def Configure(self):
-        self.output = self.GetParameter("output")
-        self.inputmap = self.GetParameter("inputmap")
-        self.PEthreshold = self.GetParameter("PEthreshold")
-        self.CutNotTriggered = self.GetParameter('CutNotTriggered')
-        self.SingleDOMCoincidenceN = self.GetParameter("SingleDOMCoincidenceN")
-        self.SingleDOMCoincidenceWindow = self.GetParameter(
-            "SingleDOMCoincidenceWindow"
-        )
+        self.output                     = self.GetParameter("output")
+        self.inputmap                   = self.GetParameter("inputmap")
+        self.trigger_map                = self.GetParameter("trigger_map")
+        self.PEthreshold                = self.GetParameter("PEthreshold")
+        self.CutNotTriggered            = self.GetParameter('CutNotTriggered')
+        self.SingleDOMCoincidenceN      = self.GetParameter("SingleDOMCoincidenceN")
+        self.SingleDOMCoincidenceWindow = self.GetParameter("SingleDOMCoincidenceWindow")
 
         self.FRAME_NUM = 1 ########################################################################################################
 
@@ -56,7 +54,7 @@ class DOMTrigger(icetray.I3ConditionalModule):
         # print("DOM Trigger Start")
         # print(self.FRAME_NUM) ########################################################################################################
 
-        PulseSeriesMap = frame["triggerpulsemap"]
+        PulseSeriesMap = frame[self.trigger_map]
         DOMCoincidence_dict = {}
         DOMPulseCount = {}
         DOMPMTCount = {}
@@ -71,25 +69,8 @@ class DOMTrigger(icetray.I3ConditionalModule):
             DOMPMTCount[omkey]         = set()
             pulses                     = PulseSeriesMap[omkey]
             lookback                   = 0
-            # if len(pulses) > 12 :
-            #    averagetime = 0.0
-            #    for i, pulse in enumerate(pulses) :
-            #        averagetime += pulse.time
-            #    averagetime /= len(pulses)
-            #    minbunchtime = 9999999.0
-            #    maxbunchtime = 0.0
-            #    for i,pulse in enumerate(pulses) :
-            #        if abs(pulse.time-averagetime)<300. :
-            #            minbunchtime  = min(minbunchtime,pulse.time)
-            #            maxbunchtime = max(maxbunchtime,pulse.time)
-            #
-            #        DOMCoincidence_time[omkey] = [minbunchtime,maxbunchtime]
-            #        DOMCoincidence_ncoin[omkey] = [4,4]
-            #        DOMCoincidence_pmts[omkey] = [4,4]
-            #    continue
-            # print("looping " + str(len(pulses)))
+
             for i, pulse in enumerate(pulses):
-                # print(str(i) +" "+str(lookback)+" "+str(len(pulses)))
                 pmt         = int(pulse.width)
                 key_time    = int(pulse.time)
                 actual_time = pulse.time
@@ -99,11 +80,7 @@ class DOMTrigger(icetray.I3ConditionalModule):
                     DOMCoincidence_dict[omkey][key_time]['pmt_time_info'] = {(pmt, actual_time)}
                     DOMPMTCount[omkey].add(pmt)
                 else:
-                    # print(self.FRAME_NUM) ########################################################################################################
-                    # print('PULSE TIME ALREADY IN DICT')
-                    # print(key_time)
-                    # print(actual_time)
-                    # print(pulse.width)
+
                     DOMPMTCount[omkey].add(pmt)
                     # [pmts] is a set so we can add no problem and not get duplicates but actual times
                     # will probably always be different so it won't add a pmt but will add a time if
@@ -121,8 +98,7 @@ class DOMTrigger(icetray.I3ConditionalModule):
                         if backpulse_pmt not in DOMCoincidence_dict[omkey][key_time]['pmts']:
                             DOMCoincidence_dict[omkey][key_time]['pmts'].add(backpulse_pmt)
                             DOMCoincidence_dict[omkey][key_time]['pmt_time_info'].add((backpulse_pmt, backpulse.time))
-                        # DOMCoincidence_dict[omkey][key_time].add((int(backpulse.width), backpulse.time))
-            # print(DOMPMTCount[omkey])
+
             if len(DOMPMTCount[omkey]) >= self.SingleDOMCoincidenceN:
                 times     = list()
                 hit_times = list()
@@ -130,16 +106,12 @@ class DOMTrigger(icetray.I3ConditionalModule):
                 pmts      = list()
                 for time in DOMCoincidence_dict[omkey].keys():
                     if len(DOMCoincidence_dict[omkey][time]['pmts']) < self.SingleDOMCoincidenceN:
-                        # print('NO COINCIDENCE') #################################################
                         continue
-                    times.append(time) ################################################# put here instead of commented out below...
+                    times.append(time)
                     coinc.append(len(DOMCoincidence_dict[omkey][time]['pmts']))
                     for pmt, hit_time in DOMCoincidence_dict[omkey][time]['pmt_time_info']:
-                        # print(pmt)
-                        # print(hit_time)
                         pmts.append(int(pmt))
                         hit_times.append(hit_time)
-                        #times.append(time)####################################### we need this to be the actual hit time...
                 if len(times) > 0:
                     DOMCoincidence_time[omkey]      = times
                     DOMCoincidence_hit_times[omkey] = hit_times
@@ -151,18 +123,12 @@ class DOMTrigger(icetray.I3ConditionalModule):
             if len(DOMCoincidence_ncoin) == 0:
                 self.FRAME_NUM += 1 ########################################################################################################
                 return
-            # total_triggers = 0
-            # for omkey in DOMCoincidence_ncoin:
-            #     total_triggers += len(DOMCoincidence_ncoin[omkey])
-                
-            # if total_triggers == 0:
-            #     return
 
 
-        frame["DOMTrigger_time" + self.output]      = DOMCoincidence_time
-        frame["DOMTrigger_ncoin" + self.output]     = DOMCoincidence_ncoin
-        frame["DOMTrigger_pmts" + self.output]      = DOMCoincidence_pmts
-        frame["DOMTrigger_hit_times" + self.output] = DOMCoincidence_hit_times
+        frame["DOMTrigger_Time" + self.output]      = DOMCoincidence_time
+        frame["DOMTrigger_NCoin" + self.output]     = DOMCoincidence_ncoin
+        frame["DOMTrigger_PMTs" + self.output]      = DOMCoincidence_pmts
+        frame["DOMTrigger_HitTimes" + self.output] = DOMCoincidence_hit_times
         # print("DOM Trigger End")
 
         self.PushFrame(frame)
