4410606649b61f0ae4e6f418a23c89e7457cf9a1
Caleb  Miller <calebmiller@uvic.ca>
2025-07-22 14:57:33 -0700

    Update DOMAcceptance.py
    possible bug

--- DOM/DOMAcceptance.py
+++ DOM/DOMAcceptance.py
@@ -60,7 +60,7 @@ class DOMAcceptance(icetray.I3ConditionalModule):
             new_omkey = NoPMTKey(omkey)
 
             # collect the photons that hit a particular OM
-            photon_list           = np.array(photon_map[omkey])
+            photon_list           = np.array(photon_map[omkey]) #SHOULD THIS BE NEW_OMKEY??????? BUG
             hit_photons, hit_pmts = self.module.apply_acceptance_cut(photon_list)
 
             # add these hits to the new photon map and add
