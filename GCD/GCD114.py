154aa30364ed3927fbb1e261925c20d97c149c78
Bennett <bennettwinnickylewis@gmail.com>
2025-08-01 09:56:59 -0600

    Added subdetector labels for POM and PCAL. Now will raise error if no input csv is provided

--- GCD/GCD114.py
+++ GCD/GCD114.py
@@ -4,20 +4,21 @@ import numpy as np
 import argparse
 import gcdHelpers
 import csv
-import warnings
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i","--input",  type=str, default="test_pos.csv", help="csv file to read")
+parser.add_argument("-i","--input",  type=str, default=None, help="csv file to read in x and y positions of lines")
 parser.add_argument("-o","--output", type=str, default="out", help="i3 file name to write")
 parser.add_argument("-d","--noms",  type=int, default=20, help="OMs per string.")
 parser.add_argument("-r","--domradius", type=int, default=216, help='Radius of dom. Defaults to 0.216 m')
-# parser.add_argument("-p", "--npmts", type=int, default=16, help="PMTs per DOM.")
 parser.add_argument("-l", "--mooringlength", type=int, default=1000, help="Length of the mooring")
-# parser.add_argument("-s", "--omsequence", type=list, default=['pom'], help="Repeating sequence of pom or pcal going from the bottom of the line to the top. Default is all poms")
-parser.add_argument("-s", "--omsequence", type=list, default=['pom','pom','pom','pom','pom','pcal','pom','pom','pom','pom','pom','pom','pcal','pom','pom','pom','pom','pom','pom'], help="Repeating sequence of pom or pcal going from the bottom of the line to the top. Default is all poms")
+parser.add_argument("-s", "--omsequence", type=list, default=['POM'], help="Repeating sequence of POM or PCAL going from the bottom of the line to the top. Default is all POMs")
+# The below sequence is the order that PONE-1 will have
+# parser.add_argument("-s", "--omsequence", type=list, default=['POM','POM','POM','POM','POM','PCAL','POM','POM','POM','POM','POM','POM','PCAL','POM','POM','POM','POM','POM','POM'], help="Repeating sequence of POM or PCAL going from the bottom of the line to the top. Default is all POMs")
 
 
 args = parser.parse_args()
+if args.input==None:
+    raise("No input csv provided. Please provide correct file")
 
 outfileName = (
     "PONE_" + str(args.output)+".i3.gz"
@@ -26,91 +27,6 @@ outfileName = (
 outfile = dataio.I3File(outfileName, "w")
 domsPerString = args.noms
 omsequence = args.omsequence
-
-# MOVE THIS PART TO A MODULE TO LOAD IN. MAYBE INTO POMUtility?
-from Utilities.POMUtility import POMProperties
-pom_properties = POMProperties()
-pmt_vectors = np.array([pom_properties.GetPMTDirection(i+1) for i in range(16)])
-PMT_orientations = np.array([dataclasses.I3Orientation(dataclasses.I3Direction(pmt_vectors[i][0],pmt_vectors[i][1],pmt_vectors[i][2])) for i in range(len(pmt_vectors))])
-
-
-
-def AddPOM(geomap,position,string,om,separation=0.0785,pom_radius=0.216,npmts=16):
-    '''
-    This function takes a geomap, xyz position, string number, and om number and adds a POM to that position. 
-    The POM is make of two different spheres each corresponding to a different hemisphere. 
-    At the moment, this model has each hemisphere facing the x direction and does not have the ability to rotate.
-
-    Parameters
-    ----------
-    geomap :: dataclasses.I3OMGeoMap() object
-        This geomap stores the geometry information which will be modified by this function.
-    position :: array like
-        position = [x, y, z] XYZ position of the added POM.
-    string :: int
-        String number.
-    om :: int
-        OM number.
-    separation :: float (default = 0.0785)
-        Separation of the centre of the two hemispheres in meters.
-    pom_radius :: float (default = 0.216)
-        Radius of each hemistphere in meters.
-    npmts :: int (default = 16)
-        Number of PMTs in the POM.
-    '''
-    x,y,z = position
-    for k in range(2):
-        omGeometry = dataclasses.I3OMGeo()
-        omGeometry.omtype = dataclasses.I3OMGeo.OMType.mDOM
-        # omGeometry.area = area
-        omGeometry.area = 4*np.pi*pom_radius**2
-        omGeometry.position = dataclasses.I3Position(
-            x+separation*(-1)**(k+1), y, z
-        )
-        for j in range(int(npmts/2)):
-            omGeometry.orientation = PMT_orientations[int(j+k*npmts/2)]
-            omkey = OMKey(string, om, int(j+k*npmts/2)+1)
-
-            geomap[omkey] = omGeometry
-
-def AddPCAL(geomap,position,string,om,separation=0.0785,pcal_radius=0.216,npmts=8):
-    '''
-    This function takes a geomap, xyz position, string number, and om number and adds a POM to that position. 
-    The POM is make of two different spheres each corresponding to a different hemisphere. 
-    At the moment, this model has each hemisphere facing the x direction and does not have the ability to rotate.
-
-    Parameters
-    ----------
-    geomap :: dataclasses.I3OMGeoMap() object
-        This geomap stores the geometry information which will be modified by this function
-    position :: array like
-        position = [x, y, z] XYZ position of the added PCAL.
-    string :: int
-        String number.
-    om :: int
-        OM number.
-    separation :: float (default = 0.0785)
-        Separation of the two hemispheres in meters.
-    pcal_radius :: float (default = 0.216)
-        Radius of each hemistphere in meters.
-    npmts :: int (default = 8)
-        Number of PMTs in the PCAL.
-    '''
-    warnings.warn("AddPCAL NOT READY YET, NEED TO BE ABLE TO ADD FLASHER TO PROPER LOCATION. HOW DO WE WANT TO NUMBER THE PCAL PMTS?")
-    x,y,z = position
-    for k in range(2):
-        omGeometry = dataclasses.I3OMGeo()
-        omGeometry.omtype = dataclasses.I3OMGeo.OMType.mDOM
-        # omGeometry.orientation = orientation
-        # omGeometry.area = area
-        omGeometry.area = 4*np.pi*pcal_radius**2
-        omGeometry.position = dataclasses.I3Position(
-            x+separation*(-1)**(k+1), y, z
-        )
-        for j in range(int(npmts/2)):
-            omGeometry.orientation = PMT_orientations[int(j+k*8+4)]
-            omkey = OMKey(string, om, int(j+k*npmts/2)+5)
-            geomap[omkey] = omGeometry
 
 def generateGeometry():
     '''
@@ -132,12 +48,12 @@ def generateGeometry():
         for m in range(domsPerString):
             loc = [xpositions[i], ypositions[i], depth[m]]
             string_num = i+1; om_num = m+1
-            if omsequence[m%len(omsequence)]=='pom':
-                AddPOM(geomap,loc,string_num,om_num)
-            elif omsequence[m%len(omsequence)]=='pcal':
-                AddPCAL(geomap,loc,string_num,om_num)
+            if omsequence[m%len(omsequence)]=='POM':
+                gcdHelpers.AddPOM(geomap,loc,string_num,om_num)
+            elif omsequence[m%len(omsequence)]=='PCAL':
+                gcdHelpers.AddPCAL(geomap,loc,string_num,om_num)
             else:
-                raise Exception("Unknown optical module type specified. Type can be 'pom' or 'pcal'") 
+                raise Exception("Unknown optical module type specified. Type can be 'POM' or 'PCAL'") 
     
     return geomap
 
@@ -165,12 +81,11 @@ with open(args.input, newline='') as csvfile:
 orientation = dataclasses.I3Orientation(0, 0, 1, 1, 0, 0) # Making the OM oriented upwards. Don't know if this will change anything
 
 geometry = dataclasses.I3Geometry()
-
 geometry.start_time = gcdHelpers.start_time
 geometry.end_time = gcdHelpers.end_time
 geomap = generateGeometry()
 geometry.omgeo = geomap
-
+# This adds all of the data we passed to the function into a geometry frame
 gframe = icetray.I3Frame(icetray.I3Frame.Geometry)
 cframe = gcdHelpers.generateCFrame(geometry)
 dframe = gcdHelpers.generateDFrame(geometry)
@@ -187,18 +102,17 @@ for dom in geomap.keys():
     module.module_type = dataclasses.I3ModuleGeo.ModuleType.mDOM
     module.orientation = orientation
     module.pos = dataclasses.I3Position(
-            xpositions[dom.string-1], xpositions[dom.string-1], depthlist[dom.om-1]
+            xpositions[dom.string-1], ypositions[dom.string-1], depthlist[dom.om-1]
         )
-    # module.radius = np.sqrt(geomap[dom].area / (4.0 * np.pi))
     module.radius = args.domradius
     modgeomap[mkey] = module
     ind+=1
 gframe["I3ModuleGeoMap"] = modgeomap
+# Setting subdetector labels to be 
 subdetec = dataclasses.I3MapModuleKeyString()
 for dom in geomap.keys():
     mkey = dataclasses.ModuleKey(dom.string, dom.om)
-    subdetec[mkey] = "Upgrade"
-
+    subdetec[mkey] =omsequence[dom.om%len(omsequence)]
 gframe["Subdetectors"] = subdetec
 
 gframe["StartTime"] = gcdHelpers.start_time
